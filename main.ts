54e625c596c0046013299e02cb63a58966853725
Tomasz Kula <tomaszkula07@gmail.com>
2016-07-31 06:03:30 +0200

    Auto completes with correct cursor placement and merging snippets

--- main.ts
+++ main.ts
@@ -1,49 +1,95 @@
-
-class Autocomplete(asd)  {
-    export function test() {}
+interface autocompleteParams {
+  input: string,
+  position: number,
+  customSnippets?: {}
 }
 
-/// <reference path="typings/jquery.d.ts" />
-let AUTOCOMPLETE = {
-    'PFR' : 'preflop raiser',
-    'BTN' : 'button'
-}
 
-let TAB_KEY = 9;
-let input = $('#read');
+class Autocomplete  {
+    private _snippets: {} = {
+        'pfr' : 'preflop raiser'
+    }
 
-function createNewString(input:string, position: number): string{
-    let firstHalf = input.substring(0, position);
-    let secondHalf = input.substring(position);
+    private _cursorPlacement: number;
+    private _input: string;
+    private _position: number;
 
-    let arrOfWords: string[] = firstHalf.split(' ');
-    let word = arrOfWords.pop();
-    firstHalf = arrOfWords.join(' ');
+    constructor(obj: autocompleteParams ) {
+      this._cursorPlacement = obj.position;
+      this._position = obj.position;
+      this._input = obj.input;
+      this.mergeSnippets(obj.customSnippets);
+    }
 
-    word = (AUTOCOMPLETE[word] === undefined) ? word : AUTOCOMPLETE[word];
+    get snippets () {
+        return this._snippets;
+    }
 
-    return firstHalf + ' ' +  word + secondHalf;
+    get cursorPlacement() {
+      return this._cursorPlacement;
+    }
+
+    mergeSnippets(customSnippets: {} ): void{
+      this._snippets = customSnippets ? Object.assign(this._snippets, customSnippets) : this._snippets;
+    }
+
+    getNewString(): string {
+      let firstHalf = this._input.substring(0, this._position);
+      let secondHalf = this._input.substring(this._position);
+
+      // removing last word from first half
+      let arrOfWords: string[] = firstHalf.split(' ');
+      let word = arrOfWords.pop();
+      firstHalf = arrOfWords.join(' ');
+
+      // checking for snippet
+      word = this.checkForSnippet(word);
+
+      return (firstHalf + ' ' +  word + secondHalf).trim();;
+    }
+
+
+    // if found calculating new cursor position and assigning new word
+    checkForSnippet(word: string) {
+      if (this._snippets[word] !== undefined) {
+        let oldWordLength = word.length;
+        word =  this._snippets[word]
+        let newWordLength = word.length;
+        this._cursorPlacement += newWordLength - oldWordLength;
+      }
+      return word;
+    }
+
 }
 
 function handleInput(e) {
+    let TAB_KEY = 9;
     if(e.which == TAB_KEY) {
         e.preventDefault(); 
         e.stopPropagation();
 
+        // geting values from the DOM
+        let input = $('#read');
         let inputString: string = input.val();
         let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("read")
         let cursorPosition: number = inputElement.selectionStart
 
-        let result = createNewString(inputString, cursorPosition);
-        result = result.trim();
-        
+        let customSnippets = {
+          'btn' : 'button'
+        }
+
+        // autocomplete class searches for snippets
+        let ac = new Autocomplete( {input: inputString, position: cursorPosition, customSnippets: customSnippets  })
+        let result:string = ac.getNewString();
+        let newCursorPosition:number = ac.cursorPlacement;
+
+        // printing results and placing the cursor
         input.val(result);
         input.select();
-        inputElement.setSelectionRange(cursorPosition, cursorPosition);
+        inputElement.setSelectionRange(newCursorPosition, newCursorPosition);
 
     }
 }
-let ac = Autocomplete
 
-$('#read').val('Lorem pfr dolor sit amet');
- input.on('keydown', handleInput);
+$('#read').val('Lorem pfr btn sit amet');
+$('#read').on('keydown', handleInput);
